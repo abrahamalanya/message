759460ba3372d3fec5741c982148f14257fcfd4f
Abraham Alanya <66879686+abrahamalanya@users.noreply.github.com>
2022-01-27 17:51:09 -0500

    update 1.0

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var typed = new Typed('#typed', {
-    strings: [
-        'Yo no sabía que llegarías',
-        'Yo no sabía que se podía amar tanto sin esperar nada a cambio',
-        'Yo no sabía de las horas que podía pasar contemplando tu rostro',
-        'Yo no sabía de los amaneceres que dependen de tu despertar',
-        'No sabía nada y ahora aprendo',
-        'Ahora vivo por fin.',
-        '',
-        'Gracias.',
-        ''
-    ],
-    stringsElement: null,
-    typeSpeed: 100,
-    startDelay: 0,
-    backSpeed: 20,
-    smartBackspace: false,
-    loop: true,
-    loopCount: Infinity,
+var message = new Typed("#message-text", {
+  strings: [
+    "Yo no sabía que llegarías",
+    "Yo no sabía que se podía amar tanto sin esperar nada a cambio",
+    "Yo no sabía de las horas que podía pasar contemplando tu rostro",
+    "Yo no sabía de los amaneceres que dependen de tu despertar",
+    "No sabía nada y ahora aprendo",
+    "Ahora vivo por fin.",
+    "",
+    "Gracias ♥.",
+    ""
+  ],
+  stringsElement: null,
+  typeSpeed: 100,
+  startDelay: 0,
+  backSpeed: 20,
+  smartBackspace: false,
+  loop: true,
+  loopCount: Infinity
 });
